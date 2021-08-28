71e9a5c36bb6172daab155f238995fcad1a4f80c
Hayden-Haun <haydenhaun@gmail.com>
2021-08-28 11:42:35 -0600

    notes Router now includes a readFile function and pulls data from db.json

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,7 +1,12 @@
 const notes = require("express").Router();
+const util = require("util");
+const fs = require("fs");
+
+const readFromFile = util.promisify(fs.readFile);
 
 notes.get("/", (req, res) => {
   console.log(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 module.exports = notes;
